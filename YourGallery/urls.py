c1138e86ab7e8af956966e5005f27ea38fe59558
Sunil Yadav <sunilyadavwork2003@gmail.com>
2022-05-02 11:57:54 +0530

    Added if functionality in project urls for static files

--- YourGallery/urls.py
+++ YourGallery/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
@@ -21,4 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',include('mygallery.urls')),
     path('admin/', admin.site.urls),
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
